1a9e96170bec9cfb6c1ab7aff9ab20f8efabf8c5
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-06-09 06:56:22 +0900

    api restTemplate get 메서드 구현

---
+++ src/main/java/com/example/client/dto/UserResponse.java
@@ -0,0 +1,16 @@
+package com.example.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserResponse {
+
+    private String name;
+    private int age;
+}
